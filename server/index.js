7d67d41353cd6caf163fb39cb7dfc69fe49cb33c
kasimibrhaim <umisak.kasim@gmail.com>
2022-08-14 01:04:32 +0300

    new commit

--- server/index.js
+++ server/index.js
@@ -4,12 +4,16 @@ const {graphqlHTTP} = require('express-graphql');
 const schema = require('./schema/schema')
 const connectDB = require('./config/db')
 require('dotenv').config()
+const cors = require('cors')
 const port = process.env.PORT || 5000;
 
 const app = express();
 
 //connect to database
 connectDB();
+
+app.use(cors());
+
 app.use('/graphql', graphqlHTTP({
     schema,
     graphiql: process.env.NODE_ENV === "development"   
